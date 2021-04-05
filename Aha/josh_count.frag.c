05f534898f2dc33905c83450c6e1ebd276e224b3
Joshua Boe <jboe@calpoly.edu>
2021-04-05 13:28:30 -0700

    added 'josh' frag files

---
+++ Aha/josh_count.frag.c
@@ -0,0 +1,13 @@
+/* josh_count.frag.c */
+/* ops to yield 1000 */
+
+#include "aha.h"
+
+int userfun(int x)
+{
+  int i;
+  for (i = 0; i < 513; i++) {
+
+  }
+  return i;
+}
---
+++ Aha/josh_lt.frag.c
@@ -0,0 +1,9 @@
+/* josh_count.frag.c */
+/* ops to yield 1000 */
+
+#include "aha.h"
+
+int userfun(int x)
+{
+  return (x < 65538);
+}
